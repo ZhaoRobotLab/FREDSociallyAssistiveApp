8eb455feb9fee8ff27df62dcc8e4aaf52745153b
Clay Shubert <clayshubert@gmail.com>
2023-01-28 18:06:50 -0500

    Updated index.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
