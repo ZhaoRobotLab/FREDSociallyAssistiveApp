c79ecca26f9364ca74f021a0aef9b7c686b85e6f
Clay Shubert <clayshubert@gmail.com>
2023-01-29 22:16:19 -0500

    Added to app.py lines with Firebase connections

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+import firebase_admin
 from flask import Flask, request
+from firebase_admin import credentials
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,5 +12,11 @@ def home():
 def greet():
     name = request.args.get('name')
     return f'Hello, {name}!'
+
+
 if __name__ == '__main__':
     app.run()
+    cred = credentials.Certificate("key.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    
