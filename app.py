2bb0d5392c77f17841fb8e0f2416564702d86736
Clay Shubert <clayshubert@gmail.com>
2023-02-10 10:27:14 -0500

    Basic user data now added in database w/ register

--- app.py
+++ app.py
@@ -44,8 +44,10 @@ def logout():
 @app.route('/register', methods =['GET', 'POST'])
 def register():
     msg = ''
-    if request.method == 'POST' and 'confirmPassword' in request.form and 'password' in request.form and 'email' in request.form :
+    if request.method == 'POST' and 'fullname' in request.form and 'userid' in request.form and 'confirmPassword' in request.form and 'password' in request.form and 'email' in request.form :
         #Grab fields from register page
+        fullname = request.form['fullname']
+        userid = request.form['userid']
         email = request.form['email']
         password = request.form['password']
         confirmPassword = request.form['confirmPassword']
@@ -53,17 +55,30 @@ def register():
         #Error check user entry
         if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             msg = 'Invalid email address !'
+        elif len(userid) >16:
+            msg = 'User ID must be less than 16 characters !'
+        elif re.match(r'[^a-zA-Z0-9]', userid):
+            msg = 'User ID must be alphanumeric !'
+        elif re.match(r'[^a-zA-Z0-9]+[^ ]', fullname):
+            msg = 'Full name must be alphanumeric !'
         elif password != confirmPassword:
             msg = 'Passwords do not match !'
-        elif not confirmPassword or not password or not email:
-            msg = 'Please fill out all fields !'
         elif len(password) < 8: 
             msg = 'Password must be at least 8 characters !'
+        elif not confirmPassword or not password or not email or not userid or not fullname:
+            msg = 'Please fill out all fields !'
         else:
-            #Create user in Firebase otherwise return error
+            #Create user in Firebase otherwise return error if registration unsuccessful
             try: 
                 auth.create_user_with_email_and_password(email, password)
                 msg = 'Account created successfully !'
+                #Add user to database
+                data = {
+                    'name': fullname,
+                    'userid': userid,
+                    'email': email,
+                }
+                db.collection('users').document(userid).set(data)
                 return render_template('login.html', msg = msg)
             except:
                 msg = 'Account Creation Failed !'
